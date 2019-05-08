1e7678f6c3e5725706b76c4972bfecbf634c2c01
hamadanaoto <teiru_3@yahoo.co.jp>
2019-05-08 22:22:08 +0900

    change tablooking version

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -14,7 +14,7 @@ require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
-require "src"
+require "tablooking"
 
 module Dummy
   class Application < Rails::Application
